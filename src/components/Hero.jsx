df2eaea6e623d1390431d9eddb4121b3b06ae8a0
Hachikoi-the-creator <facka72@gmail.com>
2022-03-07 18:11:39 -0600

    typical worked :D

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import "../sass/hero.scss";
+import Typical from "react-typical";
 
 export default function Hero() {
   return (
     <section className="hero">
       <div className="content">
         <h1>
-          Web Developer <span>Adan Moreno</span>
-          -lifelong learner -Programmer -Web developer -Pythonista -JScripter
+          Hi 👋! I'm Adan Moreno
+          <Typical
+            steps={[
+              "Programmer 💻",
+              1000,
+              "Web Developer 🕸️",
+              100,
+              "Lifelong Learner 🧠",
+              1000,
+            ]}
+            loop={Infinity}
+            wrapper="p"
+          />
+          {/* -lifelong learner -Programmer -Web developer -Pythonista -JScripter */}
         </h1>
       </div>
     </section>
