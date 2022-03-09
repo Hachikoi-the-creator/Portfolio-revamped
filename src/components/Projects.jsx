aa704f8927ebc4e1ade496499f5b71225e8010e7
Hachikoi-the-creator <facka72@gmail.com>
2022-03-08 20:15:52 -0600

    added icon :D

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,18 +1,21 @@
 import React from "react";
+import "../sass/projects.scss";
 
 export default function Projects() {
   return (
     <section className="projects">
       {/* project1 */}
-      <div>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
-          animi ipsa nobis sequi, hic sit totam perferendis. Sed animi quidem
-          laboriosam.
-        </p>
-      </div>
-      <div>
-        <img src="{jhonny}" alt="" />
+      <div className="group">
+        <div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti
+            animi ipsa nobis sequi, hic sit totam perferendis. Sed animi quidem
+            laboriosam.
+          </p>
+        </div>
+        <div>
+          <img src="{jhonny}" alt="img" />
+        </div>
       </div>
     </section>
   );
