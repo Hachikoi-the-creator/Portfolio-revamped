fa59d1e10571d41ff57816c5d8aca0bc32455ee9
Hachikoi-the-creator <facka72@gmail.com>
2022-04-16 14:03:04 -0500

    fixed resume svg, upgraded contact form data to direct links

--- src/components/ContacMe.jsx
+++ src/components/ContacMe.jsx
@@ -2,6 +2,8 @@ import "../sass/contactMe.scss";
 import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { useTranslation } from "react-i18next";
+import whatsApp from "../img/whats-app.svg";
+import emailApp from "../img/email-app.svg";
 
 export default function ContactMe() {
   const [disable, setDisable] = useState(false);
@@ -57,22 +59,26 @@ export default function ContactMe() {
             id={`message ${"a"}`}
             placeholder={t("contact form mess-place")}
           />
-          <div class="container">
-            <div class="center">
-              <button class="btn" type="submit" id={disable ? "diss-btn" : ""}>
+          <div className="container">
+            <div className="center">
+              <button
+                className="btn"
+                type="submit"
+                id={disable ? "diss-btn" : ""}
+              >
                 <svg
                   width="180px"
                   height="60px"
                   viewBox="0 0 180 60"
-                  class="border"
+                  className="border"
                 >
                   <polyline
                     points="179,1 179,59 1,59 1,1 179,1"
-                    class="bg-line"
+                    className="bg-line"
                   />
                   <polyline
                     points="179,1 179,59 1,59 1,1 179,1"
-                    class="hl-line"
+                    className="hl-line"
                   />
                 </svg>
                 <span>{disable ? "MESSAGE SENT!" : "Send"}</span>
@@ -81,18 +87,26 @@ export default function ContactMe() {
           </div>
         </form>
         {/* --- */}
-        <div>
+        <div className="contact-info">
           <h3>{t("contact info res")}</h3>
           <a href="9uduR8Ybe">{t("contact info res2")}📎</a>
+
           <h3>{t("contact info email")}</h3>
-          <p>vip.hd777@gmail.com</p>
+          <a href="mailto:vip.hd777@gmail.com">
+            <img src={emailApp} alt="email lil icon uwu" />
+          </a>
+
           <h3>Whatsapp</h3>
-          <p>+52 871 510 9227</p>
+          <a href="https://wa.me/8715109227?text=I'm%20think%20you're%20awesome">
+            <img src={whatsApp} alt="whatsapp lil icon owo" />
+          </a>
+
           <h3>{t("contact info phone")}</h3>
           <p>+52 618 334 4744</p>
+
           <h3>{t("contact info pos")}</h3>
-          <p>Mexico 🇲🇽</p>
-          <p className="signature">{t("contact info sign")}</p>
+          <p>Mexico 🦅</p>
+          <h4 className="signature">{t("contact info sign")}</h4>
         </div>
       </section>
     </section>
